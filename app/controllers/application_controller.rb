0fcc895b97fcc34669678328e3f4dc95e6be752f
xXxMONOxXx <mishastoma57@gmail.com>
2021-11-24 18:22:06 +0300

    fixed usernames in db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,12 @@ class ApplicationController < ActionController::Base
     		redirect_to(request.referrer || root_path)
   		end
 	end
+	
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
+  	protected
+	
 	def configure_permitted_parameters
-		added_attrs = [:username, :email, :password, :password_confirmation, :remeber_me]
-		devise_parameter_sanitaser.permit :sign_up, keys: added_attrs
-		devise_parameter_sanitaser.permit :accout_update, keys: added_attrs
-
-	end
+    	devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  	end
 end
