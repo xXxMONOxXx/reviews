6072aeaca96bd224316b6c9e3bda0eae5ab200be
xXxMONOxXx <mishastoma57@gmail.com>
2021-11-24 09:06:16 +0300

    styling and dark mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	before_action :set_theme
+
+	def set_theme
+  		if params[:theme].present?
+    		theme = params[:theme].to_sym
+    		cookies[:theme] = theme
+    		redirect_to(request.referrer || root_path)
+  		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'profile/index'
+  match '/users',   to: 'users#index',   via: 'get'
   devise_for :users
   resources :reviews
-
+  
   root 'reviews#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
